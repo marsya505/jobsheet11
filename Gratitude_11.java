b989f3fc7cb5c378596ad4d3e4fc67e68904894f
marsya505 <marsyazulfazahrani0@gmail.com>
2024-12-02 11:15:12 +0700

    Experiment 1

---
+++ Gratitude_11.java
@@ -0,0 +1,9 @@
+public class Gratitude_11 {
+    public static void sayThankyou() {
+    System.out.println("Thankyou for being the best teacher in the world.\n"+
+    "You inspired in me a love for learning and made me feel like I cloud ask you anything.");
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}
