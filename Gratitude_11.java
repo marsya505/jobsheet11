034ef58a868e5ca7bef782a886c2afa9cdd5b45d
marsya505 <marsyazulfazahrani0@gmail.com>
2024-12-04 14:22:30 +0700

    Question 2

--- Gratitude_11.java
+++ Gratitude_11.java
@@ -1,9 +1,6 @@
 public class Gratitude_11 {
-    public static void sayThankyou() {
-    System.out.println("Thankyou for being the best teacher in the world.\n"+
-    "You inspired in me a love for learning and made me feel like I cloud ask you anything.");
-    }
     public static void main(String[] args) {
-        sayThankyou();
+        System.out.println("Thankyou for being the best teacher in the world.\n"+
+    "You inspired in me a love for learning and made me feel like I cloud ask you anything.");
     }
 }
